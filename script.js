887916c5f5b93b26fdeb5ce3dfd6f36369f9865b
Ana Carvalho <anapaulacmoura@gmail.com>
2022-06-26 12:28:59 -0300

    corrigindo conflito com a master branch

--- script.js
+++ script.js
@@ -73,7 +73,6 @@ function renderizarMensagens(){
                 `;
             }
         }
-<<<<<<< HEAD
     }
 
 
@@ -118,7 +117,46 @@ function enviarMensagem(){
         console.log("deu ruim"));
 
     document.querySelector(".digitar-mensagem").value = "";
-=======
-        
->>>>>>> 16b00f2893f32ceca584fddaea06ecb7fbc4d680
+}
+function cadastrarUser(){  const nome = prompt("Qual é o seu nome?");
+  novoUser = {      name: nome  };
+ console.log("novoUser");
+
+  
+  const promise = axios.post(      "https://mock-api.driven.com.br/api/v6/uol/participants", novoUser  );
+  promise.then(buscarMensagens);  promise.catch(alertarErro);
+  }
+
+function alertarErro(error){  console.log(error.response.status);
+  if(error.response.status === 400) {      alert("Já exist  e um usuário com esse nome. Escolha outro!!");
+      cadastrarUser()  }
+ }
+
+
+
+function manterConexao (novoUser) {
+       let conexao = setInterval(function (){
+        axios.post("https://mock-api.driven.com.br/api/v6/uol/status", novoUser);
+        console.log("O usuário está online")
+       }, 5000);
+    }
+       
+       
+function enviarMensagem(){
+    const novaMensagem = document.querySelector(".digitar-mensagem").value;
+
+    const textoMensagem = {
+        from: novoUser.name,
+        to: destinatario,
+        text: novaMensagem,
+        type: visibilidade
+    }
+
+    const promiseMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", textoMensagem);
+
+    promiseMensagem.then(enviarMensagem);
+    promiseMensagem.catch(
+        console.log("deu ruim"));
+
+    document.querySelector(".digitar-mensagem").value = "";
 }
